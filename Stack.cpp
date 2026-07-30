dcfb0ae42fe1a8efe0315c5636dc25645c4e93c6
Stefan Savić <69756458+stevacode@users.noreply.github.com>
2026-07-30 17:55:04 +0200

    Initial commit

---
+++ Stack.cpp
@@ -0,0 +1,84 @@
+﻿// Stack.cpp : Using array
+//
+
+#include "Stack.h"
+using namespace std;
+
+struct Stack {
+	int size;
+	int top;
+	int* S;
+};
+
+void create(Stack* st) {
+	cout << "Enter size of stack: " << endl;
+	cin >> st->size; 
+	st->top = -1;
+	st->S = new int[st->size];
+}
+
+void Display(Stack st) {
+	for (int i = st.top; i >= 0; i--) {
+		cout << st.S[i] << " ";
+	}
+	cout << endl;
+}
+
+void push(Stack* st, int x) {
+	if (st->top == st->size - 1) {
+		cout << "Stack Overflow" << endl;
+	}
+	else {
+		st->top++;
+		st->S[st->top] = x;
+	}
+}
+
+int pop(Stack* st) {
+	int x = -1;
+	if (st->top == -1) {
+		cout << "Stack Underflow" << endl;
+	}
+	else {
+		x = st->S[st->top--];
+	}
+	return x;
+}
+
+int peek(Stack st, int index) {
+	int x = -1;
+	if (st.top - index + 1 < 0) {
+		cout << "Invalid Index" << endl;
+	}
+	else {
+		x = st.S[st.top - index + 1];
+	}
+	return x;
+}
+
+int isEmpty(Stack st) {
+	return st.top == -1;
+}
+
+int isFull(Stack st) {
+	return st.top == st.size - 1;
+}
+
+
+
+int main()
+{
+	struct Stack st;
+	create(&st);
+	push(&st, 10);
+	push(&st, 20);
+	push(&st, 30);
+	Display(st);
+	cout << "Popped element: " << pop(&st) << endl;
+	cout << "Popped element: " << pop(&st) << endl;
+	cout << "Popped element: " << pop(&st) << endl;
+	cout << "Popped element: " << pop(&st) << endl;
+	Display(st);
+	
+	return 0;
+}
