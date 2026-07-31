9143cd61f37e89763ea1efc3ac89f04a2d5f6030
Stefan Savić <69756458+stevacode@users.noreply.github.com>
2026-07-31 17:18:28 +0200

    Add ParenthesisMatching2 implementation

---
+++ ParenthesisMatching2.cpp
@@ -0,0 +1,64 @@
+#include "Stack.h"
+using namespace std;
+
+struct Node {
+	char data;
+	Node* next;
+}*top = NULL;
+
+
+void push(char x) {
+	Node* t = new Node;
+	if (t == NULL) {
+		cout << "Stack Overflow" << endl;
+	}
+	else {
+		t->data = x;
+		t->next = top;
+		top = t;
+	}
+}
+
+char pop() {
+	char x = -1;
+	if (top == NULL) {
+		cout << "Stack Underflow" << endl;
+	}
+	else {
+		Node* t = top;
+		top = top->next;
+		x = t->data;
+		delete t;
+	}
+	return x;
+}
+
+int isBalanced(char* exp) {
+	for (int i = 0; exp[i] != '\0'; i++) {
+		if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[') {
+			push(exp[i]);
+		}
+		else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
+			if (top == NULL) {
+				return 0;
+			}
+			char popped = pop();
+			if ((exp[i] == ')' && popped != '(') ||
+				(exp[i] == '}' && popped != '{') ||
+				(exp[i] == ']' && popped != '[')) {
+				return 0;
+			}
+		}
+	}
+	return top == NULL ? 1 : 0;
+}
+
+
+int main()
+{
+	char* exp = "{([a+b]*[c-d]/e}";
+	
+	cout << exp << " is " << (isBalanced(exp) ? "Balanced" : "Not Balanced") << endl;
+
+	return 0;
+}
